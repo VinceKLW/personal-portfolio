ccf1e2c26874b6c5f53f8cf57e63dbc615c6410a
vin macbook <user@vin-macbook-pro.local>
2025-06-17 13:40:01 -0400

    fixed dates

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import './Intro.css';
 import vincentImage from '../../assets/vincent_wong.jpeg';
+import Typed from 'typed.js';
 
 function Intro() {
+  const el = React.useRef(null);
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['Aspiring Full-Stack Developer.', 'Aspiring Tech Leader.', 'Aspiring Software Developer.'],
+      typeSpeed: 50,
+      backSpeed: 50,
+      smartBackspace: true,
+      loop: true,
+      backDelay: 5000
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="intro-container">
       <img className="intro-image" src={vincentImage} alt="headshot" />
       <div className="intro-text">
         <p className="intro-title">Hi, I'm Vincent 👋</p>
         <p className="intro-body">
-          Aspiring Full-Stack Developer.<br />
+          <span ref={el}></span><br />
           <span className="intro-body-sub">I also love the gym 💪</span>
         </p>
         <div className="intro-links">
